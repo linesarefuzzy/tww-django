d93c65129f23081470ccd22f076d160e69704889
Adam Konner <adam.konner@gmail.com>
2012-12-06 21:29:43 -0500

    reorganizing to decouple loans app
    (this is more standard django organization)

--- urls.py
+++ urls.py
@@ -1,25 +1,5 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
-	# Uncomment the admin/doc line below to enable admin documentation:
-	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-	# Uncomment the next line to enable the admin:
-	(r'^admin/', include(admin.site.urls)),
-
-	(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name':'accounts/login.html'}),
-	# (r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
-	(r'^accounts/logout/$', 'loans.views.logout_view'),
-)
-
-urlpatterns += patterns('loans.views',
-	(r'^$', 'home'),
-	(r'^loans/$', 'loans'),
-	(r'^loans/(?P<loan_id>\d+)/$', 'loan_detail'),
-	(r'^loans/(?P<loan_id>\d+)/lend$', 'lend_form'),
-	(r'^accounts/profile/$', 'user_profile'),
+	(r'^', include('loans.urls')),
 )
